8bfad3a0902301324f8f90f88a7e9bf0865c31ed
PEDRO PAULO PEREIRA DE OLIVEIRA <pedro.ppoliveira1@senacsp.edu.br>
2026-08-13 15:50:37 -0300

    commit inicial da estrutura padrão das pastas do frontend, com arquivos info.txt para auxiliar a compreensão

---
+++ Frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, './index.html'),
+			},
+		},
+	},
+})
+
+
+/*
+
+EXEMPLO: dentro de input, coloque a seguinte linha:
+
+'nome-da-tela': resolve(__dirname, './nome-da-pasta/index.html'),
+
+*/
