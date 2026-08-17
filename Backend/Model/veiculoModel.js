63d0226509d27661d6f39d5cf050c923aa8c9e03
JOAO PEDRO RODRIGUES DE ARAUJO <joao.praraujo3@senacsp.edu.br>
2026-08-17 17:10:36 -0300

    Atualizando repositories, model e removendo arquivos inutilizaveis

---
+++ Backend/Model/veiculoModel.js
@@ -0,0 +1,12 @@
+class Carro{
+    constructor(id, id_usuario, placa, marca, modelo, ano){
+        this.id = id;
+        this.id_usuario = id_usuario;
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+}
+
+module.exports = Carro;
--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -1,39 +1,33 @@
 //usando as ferramentas em que baixei
+require('dotenv').config();
+console.log('SENHA CARREGADA:', process.env.DB_PASSWORD);
 const express = require ("express");// biblioteca que esta criando o servidor web
 const app = express(); // esta iniciando o servidor e guarda na variavel.
- 
+const db = require('../Connection/db');
 //traz o caminho que criei
-const rotasDeUsuarios = require ('../Routes/usuarioRoutes');
+//const rotasDeUsuarios = require ('../Routes/usuarioRoutes');
  
 // transformando oq vier do front  em dados json fazendo com o node js entenda
- 
+
+app.get('/teste-conexao', async (req, res)=>{
+
+  try{
+    const [rows] = await db.query('SELECT 1 + 1 AS resultado');
+    res.status(200).json({conectado: true, resultado: rows[0].resultado});
+  }catch(erro){
+    res.status(500).json({conectado: false, erro: erro.message})
+  }
+
+
+});
+
 app.use(express.json());
  
 // qualquer requisiçao /api pra rotas
  
-app.use('/api', rotasDeUsuarios);
+//app.use('/api', rotasDeUsuarios);
  
 const PORTA = 3000;
 app.listen(PORTA, () => {
     console.log (`Servidor rodando  e escutando na porta ${PORTA}`);
-})
- 
-/*
-const express = require("express");
-const app = express();
-
-// const rotasDeUsuarios = require('../Routes/usuariosRoutes');
-
-app.use(express.json());
-
-// app.use('/api', rotasDeUsuarios);
-
-app.get('/', (req, res) => {
-  res.send('Servidor funcionando!');
 });
-
-const PORTA = 3000;
-app.listen(PORTA, () => {
-  console.log(`Servidor rodando e escutando na porta ${PORTA}`);
-});
-*/
