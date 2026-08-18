867cbf1af5a77034d5e7411d627e01e4e4160fbc
JoaoPedro1702 <joao.praraujo3@senacsp.edu.br>
2026-08-18 20:28:31 -0300

    Adicionando backend

---
+++ Backend/Controllers/dashboardController.js
@@ -0,0 +1,35 @@
+const dashboardRepository = require("../Repositories/dashboradRepository");
+
+async function buscarDash(req, res){
+    
+    try{
+        const vagas = await dashboardRepository.buscarDashboard();
+        const registro = await dashboardRepository.registro_ocupacao();
+        const entradasHoje = await dashboardRepository.entradasHoje();
+        const saidasHoje = await dashboardRepository.saidasHoje();
+        const permanecem = dashboardRepository.permanecem();
+        
+        res.status(200).json({
+            vagas: {
+                total: vagas.total,
+                livres: vagas.livres,
+                ocupadas: vagas.ocupadas, 
+            },
+            movimentacao: {
+                entradasHoje: entradasHoje,
+                saidasHoje: saidasHoje,
+                permanecem: permanecem,
+            } 
+        });
+    }catch(erro){
+        console.error(erro);
+        res.status(500).json({
+            mensagem: "Erro ao carregar dashboard"
+        });
+    }
+}
+
+module.exports = {
+    buscarDash
+};
+
