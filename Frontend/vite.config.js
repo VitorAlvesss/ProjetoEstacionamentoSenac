76dff8ca0eb185704fa96b16945268bda0b29cf7
TAINA GOMES NOVAIS <taina.gnovais@senacsp.edu.br>
2026-08-18 15:58:14 -0300

    subia a tela entrada_saida

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, './index.html'),
+				main: resolve(__dirname, './index.html'), 
+				'entra_saida': resolve(__dirname, './entra_saida/index.html'),
 			},
 		},
 	},
