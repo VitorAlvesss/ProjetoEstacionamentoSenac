a6551c0a62de521e3345fb44c270be27e6e0e335
OTAVIO AUGUSTO CANOLA DO CARMO <otavio.accarmo@senacsp.edu.br>
2026-08-20 16:19:21 -0300

    enviando alterações pasta

---
+++ Banco de Dados/CRUD/Script CRUD Vaga (Luis).sql
@@ -0,0 +1,16 @@
+
+USE estacionamentoSenac;
+ 
+CREATE TABLE tbl_vaga;
+ 
+SELECT * FROM tbl_vaga;
+ 
+INSERT INTO tbl_vaga VALUES
+();
+ 
+UPDATE tbl_vaga
+SET ... = 
+WHERE id = ...;
+ 
+DELETE FROM tbl_vaga
+WHERE id = ...;
