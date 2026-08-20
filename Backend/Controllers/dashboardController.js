65888dd098b45cfed3e275cc7d0c5a98932d6e90
JOAO PEDRO RODRIGUES DE ARAUJO <joao.praraujo3@senacsp.edu.br>
2026-08-20 15:45:17 -0300

    Adicionando promise.all

--- Backend/Controllers/dashboardController.js
+++ Backend/Controllers/dashboardController.js
@@ -4,15 +4,15 @@ const dashboardRepository = require("../Repositories/dashboradRepository");
 
 async function buscarDash(req, res){
     
-    const vagas = await dashboardRepository.buscarDashboard();
-    const registro = await dashboardRepository.registro_ocupacao();
-    const entradasHoje = await dashboardRepository.entradasHoje();
-    const saidasHoje = await dashboardRepository.saidasHoje();
-    const permanecem = await dashboardRepository.permanecem();
-    const dispositivos = await dashboardRepository.buscarDispositivoIOT();
-
     try{
-        
+            const [vagas, registro, entradasHoje, saidasHoje, permanecem, dispositivos] = await Promise.all([
+            dashboardRepository.buscarDashboard(),
+            dashboardRepository.registro_ocupacao(),
+            dashboardRepository.entradasHoje(),
+            dashboardRepository.saidasHoje(),
+            dashboardRepository.permanecem(),
+            dashboardRepository.buscarDispositivoIOT(),
+        ]);
         res.status(200).json({
             vagas: {
                 total: vagas.total,
