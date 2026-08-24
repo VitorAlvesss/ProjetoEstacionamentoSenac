50232abfa946224db5637dc9bef3aed0d28cc0e6
JOAO PEDRO RODRIGUES DE ARAUJO <joao.praraujo3@senacsp.edu.br>
2026-08-24 14:17:55 -0300

    Remove arquivos desnecessarios da dashboard

--- Backend/Routes/dashboardRoutes 1.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const dashboardController = require("../Controllers/dashboardController");
-
-router.get("/", dashboardController.buscarDash);
-
-module.exports = router;
