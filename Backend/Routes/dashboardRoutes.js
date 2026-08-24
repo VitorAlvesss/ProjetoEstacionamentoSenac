2230ac63b2c19db23e9d6418c861170467b11005
GUILHERME ALMEIDA DOS SANTOS <guilherme.asantos126@senacsp.edu.br>
2026-08-24 13:47:41 -0300

    Atualização de Rotas

--- Backend/Routes/dashboardRoutes.js
+++ Backend/Routes/dashboardRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+
 const router = express.Router();
 
-const dashboardController = require("../controller/dashboardController");
+const dashboardController = require("../Controllers/dashboardController");
 
 router.get("/", dashboardController.buscarDash);
 
