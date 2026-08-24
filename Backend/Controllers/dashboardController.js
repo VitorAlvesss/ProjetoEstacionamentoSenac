c7fd394690f95f8c375efd06ddbed908e2f8e59a
GUILHERME ALMEIDA DOS SANTOS <guilherme.asantos126@senacsp.edu.br>
2026-08-24 13:44:33 -0300

    atualização de controller

--- Backend/Controllers/dashboardController.js
+++ Backend/Controllers/dashboardController.js
@@ -1,49 +1,72 @@
-const dashboardRepository = require("../Repositories/dashboradRepository");
+const dashboardRepository = require("../Repositories/dashboardRepository");
 
+async function buscarDash(req, res) {
 
+    try {
 
-async function buscarDash(req, res){
-    
-    try{
-            const [vagas, registro, entradasHoje, saidasHoje, permanecem, dispositivos, vagasOcupadas, financeiro] = await Promise.all([
+        const [vagas, registro, entradasHoje, saidasHoje, permanecem, dispositivos, financeiro, todosRegistros, listaVagas] = await Promise.all([
+
             dashboardRepository.buscarDashboard(),
+
             dashboardRepository.registro_ocupacao(),
+
             dashboardRepository.entradasHoje(),
+
             dashboardRepository.saidasHoje(),
+
             dashboardRepository.permanecem(),
+
             dashboardRepository.buscarDispositivoIOT(),
-            dashboardRepository.registro_vagasOcupadas(),
-            dashboardRepository.financeiroHoje()
+
+            dashboardRepository.financeiroHoje(),
+
+            dashboardRepository.todos_registros(),
+
+            dashboardRepository.registro_vagasOcupadas()
+
         ]);
+
         res.status(200).json({
+
             vagas: {
                 total: vagas.total,
                 livres: vagas.livres,
-                ocupadas: vagas.ocupadas, 
+                ocupadas: vagas.ocupadas
             },
+
             movimentacao: {
                 entradasHoje: entradasHoje,
                 saidasHoje: saidasHoje,
                 permanecem: permanecem,
-                movimentacoes: registro,
+                movimentacoes: registro
             },
+
             financeiro: {
                 faturamento: financeiro.faturamento,
                 veiculos: financeiro.veiculos,
                 ticketMedio: financeiro.ticket_medio
             },
-            vagasOcupadas,
-            dispositivos
+
+            dispositivos,
+
+            todosRegistros,
+
+            listaVagas
+
         });
-    }catch(erro){
+
+    } catch (erro) {
+
         console.error(erro);
+
         res.status(500).json({
             mensagem: "Erro ao carregar dashboard"
         });
+
     }
+
 }
 
 module.exports = {
     buscarDash
 };
-
